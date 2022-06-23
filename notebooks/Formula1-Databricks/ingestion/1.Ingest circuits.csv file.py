6bc6dbedb55883a8bf5f9a09bdbbac9a3a9e3263
oezlemyildiz <yldz.ozlm@gmail.com>
2022-06-23 23:12:07 +0300

    Update: README.md notebooks

--- notebooks/Formula1-Databricks/ingestion/1.Ingest circuits.csv file.py
+++ notebooks/Formula1-Databricks/ingestion/1.Ingest circuits.csv file.py
@@ -52,7 +52,50 @@ df_circuits.printSchema()
 
 # COMMAND ----------
 
-#commit
+"""display(df_circuits.select("circuitId","circuitRef"))
+display(df_circuits.select(df_circuits.circuitId,df_circuits.circuitRef))
+display(df_circuits.select(df_circuits["circuitId"],df_circuits["circuitRef"]))
+"""
+from pyspark.sql.functions import col
+df_circuits_selected= df_circuits.select(col("circuitId").alias("circuit_Id"),
+                           col("circuitRef").alias("circuit_Ref"),
+                           col("name").alias("circuit_name"),
+                           col("location").alias("circuit_location"),
+                           col("country").alias("circuit_country"),
+                           col("lat").alias("circuit_lat"),
+                           col("lng").alias("circuit_lng")
+                          )
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, lit
+df_circuits_renammed= df_circuits_selected\
+.withColumnRenamed("circuit_lat", "circuit_latitute")\
+.withColumnRenamed("circuit_lng", "circuit_lengitute")\
+.withColumn("ingestion_date",current_timestamp())\
+.withColumn("env",lit("eargast"))
+
+# COMMAND ----------
+
+display(df_circuits_renammed)
+
+# COMMAND ----------
+
+df_circuits_renammed.write.mode("overwrite").parquet("dbfs:/mnt/formula1dlv4/processed/circuits")
+
+# COMMAND ----------
+
+df_circuits_parguet= spark.read.parquet("dbfs:/mnt/formula1dlv4/processed/circuits")
+
+# COMMAND ----------
+
+display(df_circuits_parguet)
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/formula1dlv4/processed/circuits
 
 # COMMAND ----------
 
