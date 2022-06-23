b2a924aaae95924ee134d9db40519e2bb96725b0
oezlemyildiz <yldz.ozlm@gmail.com>
2022-06-23 14:41:57 +0300

    Update: README.md notebooks

--- notebooks/Formula1-Databricks/ingestion/1.Ingest circuits.csv file.py
+++ notebooks/Formula1-Databricks/ingestion/1.Ingest circuits.csv file.py
@@ -10,15 +10,49 @@
 
 # COMMAND ----------
 
-
+"""
+df_circuits = spark.read\
+.options(header=True, inferschema=True)\
+.csv('/mnt/formula1dlv4/raw/circuits.csv')
+"""
 
 # COMMAND ----------
 
-df_circuits = spark.read.options(header="true").csv('/mnt/formula1dlv4/raw/circuits.csv')
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType,DateType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=
+     [StructField("circuitId", IntegerType(),False),
+     StructField("circuitRef", StringType(),True),
+     StructField("name", StringType(),True),
+     StructField("location", StringType(),True),
+     StructField("country", StringType(),True),
+     StructField("lat", StringType(),True),
+     StructField("lng", StringType(),True),
+     StructField("alt", StringType(),True),
+     StructField("url", StringType(),True)]
+     #StructField("ingestionDate", DateType(),False)
+)
+
+# COMMAND ----------
+
+df_circuits = spark.read\
+.options(header=True, schema=circuits_schema)\
+.csv('/mnt/formula1dlv4/raw/circuits.csv')
 
 # COMMAND ----------
 
 display(df_circuits)
+
+# COMMAND ----------
+
+#df_circuits.describe().show()
+df_circuits.printSchema()
+
+# COMMAND ----------
+
+#commit
 
 # COMMAND ----------
 
