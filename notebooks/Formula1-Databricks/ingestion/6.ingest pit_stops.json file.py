3f5f2b8e27b304f8582c7c3946fafee79d4c477a
oezlemyildiz <yldz.ozlm@gmail.com>
2022-06-24 14:04:12 +0300

    Update: README.md notebooks

---
+++ notebooks/Formula1-Databricks/ingestion/6.ingest pit_stops.json file.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+from pyspark.sql.types import StructType, StructField,IntegerType,StringType,DateType
+pit_stops_schema=StructType(fields=[
+    StructField("raceId",IntegerType(),False),
+    StructField("driverId",IntegerType(),True),
+    StructField("stop",StringType(),True),
+    StructField("lab",IntegerType(),True),
+    StructField("time",StringType(),True),
+    StructField("duration",StringType(),True),
+    StructField("milseconds",IntegerType(),True)
+    ])
+
+# COMMAND ----------
+
+df_pit_stops= spark.read\
+.schema(pit_stops_schema)\
+.option("multiLine",True)\
+.json("/mnt/formula1dlv4/raw/pit_stops.json")
+
+# COMMAND ----------
+
+display(df_pit_stops)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, col, lit, concat
+df_pit_stops_renamed= df_pit_stops.withColumnRenamed("raceId","race_id")\
+.withColumnRenamed("driverId","driver_id")\
+.withColumnRenamed("driverId","driverId")\
+.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+df_pit_stops_renamed.write.mode("overwrite").parquet("/mnt/formula1dlv4/processed/pit_stops")
+
+# COMMAND ----------
+
+display(spark.read.parquet("/mnt/formula1dlv4/processed/pit_stops"))
+
+# COMMAND ----------
+
